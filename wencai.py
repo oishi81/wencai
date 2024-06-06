48c832144579a3e5fd0b770a8e600202ab9fab6b
oishi <65519561+oishi81@users.noreply.github.com>
2024-06-06 21:43:54 +0800

    Update wencai.py
    修改wencai参数，由以前的question改为query

--- wencai.py
+++ wencai.py
@@ -7,6 +7,6 @@ keywords = os.environ.get("KEYWORD", "").split(',')
 # 遍历关键字列表进行查询和保存结果
 for keyword in keywords:
     filename = f'{keyword}.csv'
-    res = pywencai.get(question=keyword, loop=True)
+    res = pywencai.get(query=keyword, loop=True)
     res.to_csv(filename, index=False, encoding='utf-8-sig')
     print(res)
