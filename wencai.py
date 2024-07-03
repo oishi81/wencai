c42653499647f6e9a136533f8600c9300a48346a
oishi <65519561+oishi81@users.noreply.github.com>
2024-07-03 15:34:21 +0800

    Update wencai.py

--- wencai.py
+++ wencai.py
@@ -1,12 +1,16 @@
 import pywencai
 import os
+import datetime
 
 # 获取关键字列表
 keywords = os.environ.get("KEYWORD", "").split(',')
-
+now = datetime.datetime.now()
+t = now.date()
+t2 = t.strftime('%m月%d日')
 # 遍历关键字列表进行查询和保存结果
 for keyword in keywords:
     filename = f'{keyword}.csv'
-    res = pywencai.get(query=keyword, loop=True)
+    keyword2 = t2+' '+keyword
+    res = pywencai.get(query=keyword2, loop=True)
     res.to_csv(filename, index=False, encoding='utf-8-sig')
     print(res)
