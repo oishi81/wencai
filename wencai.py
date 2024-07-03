104da6b7277655440c92a62207e9652a0535083d
oishi <65519561+oishi81@users.noreply.github.com>
2024-07-03 15:45:50 +0800

    Update wencai.py

--- wencai.py
+++ wencai.py
@@ -4,7 +4,7 @@ import datetime
 
 # 获取关键字列表
 keywords = os.environ.get("KEYWORD", "").split(',')
-today = datetime.day.today()
+today = datetime.date.today()
 tomorrow = today + datetime.timedelta(days=1)
 t2 = tomorrow.strftime('%m月%d日')
 # 遍历关键字列表进行查询和保存结果
