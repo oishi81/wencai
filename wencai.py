2561bbad75cb35e6cf0682eba7b777e8885a1bbc
oishi <65519561+oishi81@users.noreply.github.com>
2024-07-03 15:44:42 +0800

    Update wencai.py

--- wencai.py
+++ wencai.py
@@ -4,12 +4,12 @@ import datetime
 
 # 获取关键字列表
 keywords = os.environ.get("KEYWORD", "").split(',')
-now = datetime.datetime.now()
-t = now.date()
-t2 = t.strftime('%m月%d日')
+today = datetime.day.today()
+tomorrow = today + datetime.timedelta(days=1)
+t2 = tomorrow.strftime('%m月%d日')
 # 遍历关键字列表进行查询和保存结果
 for keyword in keywords:
-    filename = f'{keyword}.csv'
+    filename = f'{keyword}-{tomorrow}.csv'
     keyword2 = t2+' '+keyword
     res = pywencai.get(query=keyword2, loop=True)
     res.to_csv(filename, index=False, encoding='utf-8-sig')
