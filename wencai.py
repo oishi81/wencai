f0f82b98731b5d71471b9800035c1913d39b56b3
oishi <65519561+oishi81@users.noreply.github.com>
2024-07-05 09:32:42 +0800

    Update wencai.py

--- wencai.py
+++ wencai.py
@@ -8,13 +8,13 @@ keywords = os.environ.get("KEYWORD", "").split(',')
 today = datetime.date.today()
 tomorrow = today + datetime.timedelta(days=1)
 t2 = tomorrow.strftime('%m月%d日')
-t2 = '07月05日'
+
 # 遍历关键字列表进行查询和保存结果
 for keyword in keywords:
     filename = f'{keyword}-{tomorrow}.csv'
     keyword2 = t2+' '+keyword
     res = pywencai.get(query=keyword2, loop=True)
-    columns_to_select = [0, 1, 4, 9]
+    columns_to_select = [0, 1, 5, 10]
     # 使用iloc来截取列
     selected_res = res.iloc[:, columns_to_select]
     if res is None:
